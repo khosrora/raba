d2bc212b6ced00a468c1888789c0a7c39d2dfe7d
khosrora <khosrora153333@gmail.com>
2021-10-19 09:55:37 +0330

    responsive menu

---
+++ public/js/jsWebsite.js
@@ -0,0 +1,15 @@
+// ! get items
+const iconOpen = document.getElementById("open");
+const iconClose = document.getElementById("close");
+const menu = document.querySelector(".sliding-nav");
+
+
+// ! open menu
+iconOpen.addEventListener("click", () => { 
+    menu.style.transform = "translateX(0rem)"    
+})
+
+// ! close menu
+iconClose.addEventListener("click", () => { 
+    menu.style.transform = "translateX(20rem)"    
+})
