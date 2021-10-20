8aa22a6ae5199cbcfacfbeb7d6cfaf34c6e24b0e
khosrora <khosrora153333@gmail.com>
2021-10-20 15:51:42 +0330

    set ==>  owl carousel // login register pages is DONE !!

--- public/js/jsWebsite.js
+++ public/js/jsWebsite.js
@@ -5,11 +5,33 @@ const menu = document.querySelector(".sliding-nav");
 
 
 // ! open menu
-iconOpen.addEventListener("click", () => { 
-    menu.style.transform = "translateX(0rem)"    
+iconOpen.addEventListener("click", () => {
+    menu.style.transform = "translateX(0rem)"
 })
 
 // ! close menu
-iconClose.addEventListener("click", () => { 
-    menu.style.transform = "translateX(20rem)"    
+iconClose.addEventListener("click", () => {
+    menu.style.transform = "translateX(20rem)"
 })
+
+
+// ! owl carousel
+$('.owl-carousel').owlCarousel({
+    rtl: true,
+    margin: 20,
+    nav: true,
+    lazyLoad: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+// ! owl carousel
