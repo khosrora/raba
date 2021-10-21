fabad3d90fd4d161c756dcee8d51a25721cae9c5
khosrora <khosrora153333@gmail.com>
2021-10-21 14:30:44 +0330

    user panel is DONE !!

--- public/js/jsWebsite.js
+++ public/js/jsWebsite.js
@@ -14,24 +14,3 @@ iconClose.addEventListener("click", () => {
     menu.style.transform = "translateX(20rem)"
 })
 
-
-// ! owl carousel
-$('.owl-carousel').owlCarousel({
-    rtl: true,
-    margin: 20,
-    nav: true,
-    lazyLoad: true,
-    autoplay: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
-        }
-    }
-})
-// ! owl carousel
---
+++ public/js/owlCarousel.js
@@ -0,0 +1,21 @@
+
+// ! owl carousel
+$('.owl-carousel').owlCarousel({
+    rtl: true,
+    margin: 20,
+    nav: true,
+    lazyLoad: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+// ! owl carousel
